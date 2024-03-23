2f28c980ff0c396e6ac3a1457148e06aad542f6b
Andreas Løfsgaard <andreas@lofsgaard.com>
2024-03-23 09:07:00 +0100

    all up to htmlnode

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode
+
+def main():
+    obj_one = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    print(obj_one)
+
+
+
+if __name__ == '__main__':
+    main()
